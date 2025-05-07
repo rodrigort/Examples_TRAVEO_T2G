ec7dd0604013d792f74508744a3858ff89322668
Rodrigo Rodrigues Teixeira <mosfet.01@hotmail.com>
2025-05-07 17:33:30 -0300

    Delete 02_Switch_GPIO directory

--- 02_Switch_GPIO/build/Debug/libraries_shared/mtb-pdl-cat1/drivers/source/TOOLCHAIN_GCC_ARM/cy_syslib_ext.o.d
+++
@@ -1,2 +0,0 @@
-/Users/rodrigorodriguesteixeira/Exemples_TRAVEO/02_Switch_GPIO/build/Debug/libraries_shared/mtb-pdl-cat1/drivers/source/TOOLCHAIN_GCC_ARM/cy_syslib_ext.o: \
- /Users/rodrigorodriguesteixeira/Exemples_TRAVEO/mtb_shared/mtb-pdl-cat1/release-v3.16.0/drivers/source/TOOLCHAIN_GCC_ARM/cy_syslib_ext.S
--- 02_Switch_GPIO/build/last_config/mtb-example-empty-app.hex.d
+++
@@ -1,1 +0,0 @@
-/Users/rodrigorodriguesteixeira/Exemples_TRAVEO/02_Switch_GPIO/build/APP_KIT_T2G-B-E_LITE/Debug/mtb-example-empty-app.hex
